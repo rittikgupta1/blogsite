4bb3e09dbe35776e295ea24d1a6b05643b80f0cf
Ritik Gupta <rittikgupta1@gmail.com>
2021-04-16 15:35:53 +0530

    Added blogging title and paragraph and compose.ejs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-
+//setting up packages
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -9,21 +9,47 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+let array=[];
 
+
+//setting up ejs
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
-res.render("home",{homeStartingContent:homeStartingContent})
+
+
+
+//Home route
+app.get("/", (req, res) => {
+  res.render("home", { homeStartingContent: homeStartingContent,array:array })
 });
-app.get("/about",(req,res)=>{
-  res.render("about",{aboutContent:aboutContent})
-  });
-  app.get("/contact",(req,res)=>{
-    res.render("contact",{contactContent:contactContent})
-    });
+
+
+//About route
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent })
+});
+
+//Contact route
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent })
+});
+
+//Compose route
+app.get("/compose", (req, res) => {
+  res.render("compose",)
+});
+app.post("/compose", (req, res) => {
+  let data = {
+    title: req.body.composetext,
+    content: req.body.composebody
+  };
+  array.push(data);
+  res.redirect("/");
+
+})
 
 
 
@@ -34,6 +60,7 @@ app.get("/about",(req,res)=>{
 
 
 
-app.listen(3000, function() {
+//Listen
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
